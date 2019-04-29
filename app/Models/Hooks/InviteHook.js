c1370e2e40485a1ae84dbe1a3ee7b9cb64f61889
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2019-04-29 11:54:09 -0300

    added email queue

--- app/Models/Hooks/InviteHook.js
+++ app/Models/Hooks/InviteHook.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Kue = use('Kue')
+const Job = use('App/Jobs/InvitationEmail')
 
 const InviteHook = (exports = module.exports = {})
 
@@ -12,8 +14,9 @@ InviteHook.sendInvitationEmail = async invite => {
   if (invited) {
     await invited.teams().attach(invite.team_id)
   } else {
-    // crate user account
-    console.log('create account')
-    // send email
+    const user = await invite.user().fetch()
+    const team = await invite.team().fetch()
+
+    Kue.dispatch(Job.key, { user, team, email }, { attempts: 3 })
   }
 }
