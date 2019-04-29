97ec96d3db8e1fef4025a968d35038398f50152b
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2019-04-29 11:21:47 -0300

    Invite & invite Hooks started

--- app/Controllers/Http/InviteController.js
+++ app/Controllers/Http/InviteController.js
@@ -1,11 +1,20 @@
 'use strict'
 
+const Invite = use('App/Models/Invite')
 /**
  * Resourceful controller for interacting with invites
  */
 class InviteController {
-  async store ({ request, response }) {
-    console.log(request.team)
+  async store ({ request, auth }) {
+    const invites = request.input('invites')
+
+    const data = invites.map(email => ({
+      email,
+      user_id: auth.user.id,
+      team_id: request.team.id
+    }))
+
+    await Invite.createMany(data)
   }
 }
 
